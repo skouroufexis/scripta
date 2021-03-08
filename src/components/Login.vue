9a0ec9bcb493ce09b73a2072983127d49e25765f
skouroufexis <skouroufexis@gmail.com>
2021-03-09 01:12:50 +0200

    .

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -63,8 +63,8 @@ export default {
                 //send post request with credentials
                 let request=new XMLHttpRequest();
                 
-                request.open('POST','https://scripta-app.herokuapp.com/api/login/');
-                // request.setRequestHeader('Access-Control-Allow-Methods','POST,GET,PUT,DELETE');
+                request.open('POST','https://scripta-app.herokuapp.com/api/login');
+                request.setRequestHeader('Allow','POST');
                 let data=JSON.stringify({email:this.email,password:this.password});
                 request.send(data);                  
                 let self=this;
