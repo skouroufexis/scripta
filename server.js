ce25a1ea804fdcda018fadbe5f5619ab4f0eb15e
skouroufexis <skouroufexis@gmail.com>
2021-03-09 00:32:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -44,14 +44,8 @@ connection.query(queryUpload,function(error,results){
   }
   
 })
-
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-
-var cors_proxy = require('cors-anywhere');
-
-cors_proxy.createServer(function (req, res) {
+let port=process.env.PORT||8000;
+http.createServer(function (req, res) {
 
   //handling CORS
   const headers = {
@@ -1543,7 +1537,4 @@ cors_proxy.createServer(function (req, res) {
  
   
   
-}).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
-
+}).listen(port,'0.0.0.0');
