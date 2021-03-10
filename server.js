04b3c83ca88d3d0dc122a7eaaebedf4ff81e17ea
skouroufexis <skouroufexis@gmail.com>
2021-03-10 02:10:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 8080;
 // app.use(express.static('public'));
 // app.use("/api", express.static(path.join(__dirname,"dist")));
 // app.use(express.static(__dirname+'/dist/'));
-app.use('/', serveStatic(path.join(__dirname, 'dist')));
+// app.use('/', serveStatic(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 // app.use(cors());
 // app.get(/.*/,function(req,res){
