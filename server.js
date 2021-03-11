4318595e0bfd82a7157a2f16966c4a5897c3b64a
skouroufexis <skouroufexis@gmail.com>
2021-03-11 20:32:47 +0200

    .

--- server.js
+++ server.js
@@ -203,7 +203,7 @@ app.post('/api/addNewUser',function(req,res){
 });
 
 app.post('/api/validate_password',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let query= 'SELECT * FROM users WHERE user_id=?';
     let oldPassword=req.body.oldPassword;
     let user_id=req.body.user_id;
@@ -256,7 +256,7 @@ app.post('/api/validate_password',function(req,res){
 })
 
 app.post('/api/addNewNote',function(req,res){
-  if(req.header('session_id')!='null')
+  if(req.header('session_id'))
   {let query;  
 
   let user_id=req.header('user_id');              
@@ -544,7 +544,7 @@ app.post('/api/addNewNote',function(req,res){
 
 
 app.post('/api/attachment',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let query='INSERT INTO attachments (attachment_name,note_fk) VALUES(?,?)';
     req.body.forEach(d=>{
       let attachment_name=d.attachment_name;
@@ -576,7 +576,7 @@ app.post('/api/attachment',function(req,res){
 })
 
 app.post('/api/notesTags',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_fk=req.body.user_fk;
     let tag_name=req.body.tag_name;
     let note_fk=req.body.note_fk;
@@ -659,7 +659,7 @@ app.post('/api/notesTags',function(req,res){
 
 app.put('/api/update_password',function(req,res){
   
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     
     let user_id=req.body.user_id;
     let user_password=req.body.user_password;         
@@ -696,7 +696,7 @@ app.put('/api/update_password',function(req,res){
 })
 
 app.put('/api/update-info',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_id=req.body.user_id;
     let user_name=req.body.user_name;
     let user_email=req.body.user_email;         
@@ -724,7 +724,7 @@ app.put('/api/update-info',function(req,res){
 
  //assigns a single note to a new dossier
 app.put('/api/assign-new-dossier',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_fk=req.body.user_fk;
     let note_id=req.body.note_id;
     let dossier_name=req.body.dossier_name;
@@ -767,7 +767,7 @@ app.put('/api/assign-new-dossier',function(req,res){
 })
 
 app.put('/api/dossier-name',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_id=req.body.user_id;
     let dossier_fk=req.body.dossier_fk;
     let dossier_name=req.body.dossier_name;
@@ -809,7 +809,7 @@ app.put('/api/dossier-name',function(req,res){
 })
 
 app.put('/api/note',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let note_id=req.body.note_id;
     let title=req.body.title;
     let body=req.body.body;
@@ -837,7 +837,7 @@ app.put('/api/note',function(req,res){
 
 
 app.delete('/api/delete-user',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     
     let user_id=req.body.user_id;
     let user_password=req.body.user_password;  
@@ -915,7 +915,7 @@ app.delete('/api/delete-user',function(req,res){
 });
 
 app.delete('/api/update-info',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_id=req.body.user_id;
     let user_name=req.body.user_name;
     let user_email=req.body.user_email;         
@@ -940,7 +940,7 @@ app.delete('/api/update-info',function(req,res){
 });
 
 app.delete('/api/attachment',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let attachment_id=req.body.attachment_id;
     let query = 'DELETE FROM attachments WHERE attachment_id=?';
     
@@ -964,7 +964,7 @@ app.delete('/api/attachment',function(req,res){
 });
 
 app.delete('/api/note-tag',function(req,res){
-  if(req.header('session_id')!='null')
+  if(req.header('session_id'))
   {
     let note_fk=req.body.note_fk;
     let tag_fk=req.body.tag_fk;  
@@ -991,7 +991,7 @@ app.delete('/api/note-tag',function(req,res){
   }
 })
 app.delete('/api/dossier',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let dossier_fk=data.dossier_fk;
             query='DELETE FROM notes WHERE dossier_fk=?';
             connection.query(query,[dossier_fk],function(error,results){
@@ -1040,7 +1040,7 @@ app.get('/api/navigate',function(req,res){
 app.get('/api/expiry',function(req,res){
   console.log(req.headers);
   // console.log('HEADERRRRRR:'+ req.header('session_id'));
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     
      //returns the time difference between current time and the timestamp of the stored session_id
      let query='SELECT TIMEDIFF((SELECT time FROM sessions where session_id =?),now())';
@@ -1117,7 +1117,7 @@ app.get('/api/logout',function(req,res){
 })
 
 app.get('/api/account',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_id=req.header('user_id');
     let query='SELECT * FROM users WHERE user_id=?';
     connection.query(query,[user_id],function(error,results){
@@ -1149,7 +1149,7 @@ app.get('/api/account',function(req,res){
 })
 
 app.get('/api/get-user-dossiers',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let query;
     let user_id=req.header('user_id');
     let inputValue=req.header('input_value');
@@ -1210,7 +1210,7 @@ app.get('/api/get-user-dossiers',function(req,res){
 
 
 app.get('/api/get-user-tags',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let query;
             let user_id=req.header('user_id');
             let inputValue=req.header('input_value');
@@ -1276,7 +1276,7 @@ app.get('/api/get-user-tags',function(req,res){
 
 
 app.get('/api/get-dossiers-info',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let query;
     let user_id=req.header('user_id');
     let userDossiers=[];
@@ -1342,7 +1342,7 @@ app.get('/api/get-dossiers-info',function(req,res){
 });
 
 app.get('/api/user-notes',function(req,res){
-  if(req.header('session_id')!='null')
+  if(req.header('session_id'))
   {
     let user_id=req.header('user_id');
     let dossier_id=req.header('dossier_id');
@@ -1378,7 +1378,7 @@ app.get('/api/user-notes',function(req,res){
 });
 
 app.get('/api/note-dossier',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let dossier_fk=req.header('dossier_fk');
     let query='SELECT * FROM userDossier WHERE userDossier_id=?';
     connection.query(query,[dossier_fk],function(error,results){
@@ -1402,7 +1402,7 @@ app.get('/api/note-dossier',function(req,res){
 });
 
 app.get('/api/note-attachments',function(req,res){
-  if(req.header('session_id')!='null')
+  if(req.header('session_id'))
   {
     let note_fk=req.header('note_fk');            
     let query='SELECT * FROM attachments WHERE note_fk=?';
@@ -1432,7 +1432,7 @@ app.get('/api/note-attachments',function(req,res){
 });
 
 app.get('/api/note-tags',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let note_fk=req.header('note_fk');
     let query='SELECT * FROM notesTags WHERE note_fk=?';     
 
@@ -1461,7 +1461,7 @@ app.get('/api/note-tags',function(req,res){
 });
 
 app.get('/api/user-tags',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let userTag_id=req.header('usertag_id');            
     let query='SELECT * FROM userTags WHERE userTag_id=?';     
     connection.query(query,[userTag_id],function(error,results){
@@ -1489,7 +1489,7 @@ app.get('/api/user-tags',function(req,res){
 });
 
 app.get('/api/recent-notes',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_id=req.header('user_id');
     let query='SELECT * FROM notes WHERE user_fk=? ORDER BY note_id DESC LIMIT 3';
     connection.query(query,[user_id],function(error,results){
@@ -1524,7 +1524,7 @@ app.get('/api/recent-notes',function(req,res){
 })
 
 app.get('/api/query-note',function(req,res){
-  if(req.header('session_id')!='null'){
+  if(req.header('session_id')){
     let user_id=req.header('user_id');
     let query_input=req.header('query_input');
         query_input='%'+query_input+'%';
