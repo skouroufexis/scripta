adc8f592fe896635163ad946095cfcd961776ccb
skouroufexis <skouroufexis@gmail.com>
2021-03-14 11:56:34 +0200

    .

--- src/components/New.vue
+++ src/components/New.vue
@@ -510,7 +510,7 @@ export default {
  
         // //create POST request
         let request= new XMLHttpRequest();    
-        request.open('POST','http://localhost:8080/api/addNewNote');
+        request.open('POST','https://scripta-app.herokuapp.com/api/addNewNote');
         
         //set session_id in the request header
         let session_id=localStorage.getItem('session_id');  
